6a8c534d1bdcf54c65dc969d067403056cffc1ca
cyber-arthur <arthur.nguyen@cyberses.fr>
2025-04-18 14:08:22 +0200

    Update osint.py

--- utils/osint.py
+++ utils/osint.py
@@ -1,7 +1,7 @@
 from utils.helpers import run_command
 
 def osint_harvester(domain):
-    cmd = f"python3 ~/theHarvester/theHarvester.py -d {domain} -b all -l 200"
+    cmd = f"python3 ~/cyber_diag/theHarvester/theHarvester.py -d {domain} -b all -l 200"
     out, err = run_command(cmd)
     if out:
         return {"texte": out.strip()}
