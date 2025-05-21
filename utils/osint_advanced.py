cd574e4b63568f9b2740fc0e41c9aba8e57b074b
cyber-arthur <arthur.nguyen@cyberses.fr>
2025-05-21 11:50:00 +0200

    update exporte.py osint_advanced.py

--- utils/osint_advanced.py
+++ utils/osint_advanced.py
@@ -4,26 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GREYNOISE_API_KEY = os.getenv("GREYNOISE_API_KEY")
 VT_API_KEY = os.getenv("VT_API_KEY")
-
-def check_greynoise(ip):
-    url = f"https://api.greynoise.io/v3/community/{ip}"
-    headers = {"key": GREYNOISE_API_KEY}
-    try:
-        r = requests.get(url, headers=headers, timeout=10)
-        if r.status_code == 200:
-            data = r.json()
-            return {
-                "classification": data.get("classification"),
-                "name": data.get("name"),
-                "last_seen": data.get("last_seen"),
-                "tags": data.get("tags")
-            }
-        else:
-            return {"error": f"GreyNoise HTTP {r.status_code}"}
-    except Exception as e:
-        return {"error": str(e)}
 
 def check_virustotal(domain):
     url = f"https://www.virustotal.com/api/v3/domains/{domain}"
